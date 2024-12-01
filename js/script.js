1879f9575567ba5019035edb71e69fcd8bb02f38
Novemberman90 <alexledovit@gmail.com>
2024-12-02 01:50:25 +0200

    add ourmune section

---
+++ js/script.js
@@ -0,0 +1,9 @@
+    const swiper = new Swiper(".ourmenu-slider", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      loop: true,
+      navigation: {
+      nextEl: ".ourmenu__control-next",
+      prevEl: ".ourmenu__control-prev",
+    },
+    });
