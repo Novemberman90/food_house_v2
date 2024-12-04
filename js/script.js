819d535f33a593fb151493b1c59df1c5d7a41b32
Novemberman90 <alexledovit@gmail.com>
2024-12-05 01:20:04 +0200

    add modal adn scrtip from modal

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ window.addEventListener('DOMContentLoaded', ()=>{
   const menuBtn = document.querySelector('.menu__btn');
   const mobilMenu = document.querySelector('.menu__inner');
 
+  // вызов мобильного меню
   menuBtn.addEventListener('click', ()=>{
     if (menuBtn) {
        document.body.classList.toggle('lock');
@@ -17,17 +18,42 @@ window.addEventListener('DOMContentLoaded', ()=>{
     mobilMenu.classList.remove('menu__inner--open');
   };
 
+  // закрытие мменю при нажатии мимо меную
   document.body.addEventListener('click',(e)=>{
     if (
       e.target.closest('.menu__btn') == null &&
       e.target.closest('.menu__inner') != mobilMenu) {
-        closeMenu()
+        closeMenu();
       }
   });
- 
 
-      
-    // ourmenu
+  // прокрутка от меню к секции
+  const scrollLinks = document.querySelectorAll('.menu__item-link');
+
+    scrollLinks.forEach(link => {
+      link.addEventListener('click', (e)=>{
+        e.preventDefault();
+
+        const targetId = link.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+
+        if(targetElement) {
+          // Вычисляю растояние прокрутки с учётом высоты шапки
+          const headerHeight = document.querySelector('#header').offsetHeight;
+
+          const top = targetElement.getBoundingClientRect().top + window.scrollY - headerHeight;
+
+          window.scrollTo({
+            top: top,
+            behavior: "smooth"
+          });
+          // Удаляю классы активности и закрываю меню
+          closeMenu()
+        }
+      })
+    });
+ 
+    // слайдер ourmenu
   const swiper = new Swiper(".ourmenu-slider", {
     slidesPerView: 3,
     spaceBetween: 30,
@@ -56,7 +82,7 @@ window.addEventListener('DOMContentLoaded', ()=>{
   }
   });
 
-    //review
+    // слайдер review
   var swiperReview = new Swiper(".review__slider", {
     slidesPerView: 1,
     spaceBetween: 0,
@@ -66,5 +92,84 @@ window.addEventListener('DOMContentLoaded', ()=>{
     },
   });
 
+
+  // modal
+  const button =  document.querySelectorAll('[data-modal]');
+  const modalWindow = document.querySelector('.modal');
+
+  function openModal(params) {
+    modalWindow.classList.add('modal--active');
+    document.body.style.overflow = 'hidden';
+    closeMenu();
+    clearInterval(modalTimerId);
+  }
+
+  function closeModal(params) {
+    modalWindow.classList.remove('modal--active');
+    document.body.style.overflow = '';
+  }
+
+  // открываю модалку 
+  button.forEach(btn => {
+    btn.addEventListener('click', openModal);
+  });
+  
+  modalWindow.addEventListener('click', (e)=>{
+    if (e.target === modalWindow || e.target.getAttribute('data-close') == '') {
+      closeModal();
+    }
+  });
+
+  // открыавю модальное окно при пролистывании до конца
+   function showModalByScroll(params) {
+        if (window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 10 ) {
+
+        openModal();
+        window.removeEventListener('scroll',showModalByScroll);
+        }
+      }
+      window.addEventListener('scroll',showModalByScroll);
+
+      //чтобы срабатывало закрытие при нажатии кнопки на клаве ескейп
+    document.addEventListener('keydown', (e) => {
+      if (e.code === 'Escape' && modalWindow.classList.contains('modal--active')) {
+        closeModal();
+      }
+    });
+    //вызову модальное окно через некоторе время 
+    const modalTimerId = setTimeout(openModalWindow, 60000);
+
+
+
+      // Создаю дополнительное окно типа благорадности за отправку формы 
+
+    function showThanksModal(message) { // в аргументы передаю объект message где находятся сообщения и мне нужно всеголишь выбрать нужное
+      const prevModalDialog = document.querySelector('.modal__dialog');
+
+      /* Прячу текущее */
+      prevModalDialog.classList.add('hide');
+      openModal();
+
+      /* Создаю новое и присваеваю ему класс  modal__dialog */
+      const thanksModal = document.createElement('div'); 
+      thanksModal.classList.add('modal__dialog');
+
+      /* Тперь надо сформировать вёрстку */
+      thanksModal.innerHTML = `
+      <div class="modal__content">
+        <div class="modal__close" data-close>&times;</div>
+        <div class="modal__title">${message}</div>
+      </div> `;
+
+      // и указываю куда его добавляю
+      document.querySelector('.modal').append(thanksModal);
+      setTimeout(() => {
+        thanksModal.remove(); 
+        prevModalDialog.classList.add('show');
+        prevModalDialog.classList.remove('hide');
+        closeModal();
+      }, 4000);
+    }
 });
+    
 
